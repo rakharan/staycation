fc4497789bbfce02c0a83c9eb7674a432f6c50ec
Rakha Randhikatama <randhikatamar@gmail.com>
2023-06-26 19:29:30 +0700

    Absolute path configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-
-  test: {
-    globals: true,
-    environment: 'jsdom',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
-  plugins: [react()]
+  plugins: [react()],
 });
