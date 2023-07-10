384bc9cd2e12d41d815e0bf4ac054701ab0c8433
Rakha Randhikatama <randhikatamar@gmail.com>
2023-07-10 21:49:23 +0700

    Create Checkout.jsx

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "@/parts/Header";
+
+const Checkout = () => {
+  return (
+    <div className="min-h-screen">
+      <Header isCentered />
+    </div>
+  );
+};
+
+export default Checkout;
